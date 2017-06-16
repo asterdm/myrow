0866d83e0d384b85410a0cdc3da4052b95f95e3e
asterdm <asterdm@yandex.ru>
2017-06-16 20:54:12 +0300

    настроена работа кнопок управления панели

--- backend/controllers/PanelController.php
+++ backend/controllers/PanelController.php
@@ -15,37 +15,71 @@ class PanelController extends \yii\web\Controller
         $box = Boxes::findOne($number);
         //var_dump($box);die;
         
-        $data['id'] = $box->id;
-        $data['user_cod'] = $box->user_cod;
-        $data['status'] = $box->status;
-        $data['time'] = $box->time;
-        return $this->render('manager',$data);
+//        $data['id'] = $box->id;
+//        $data['user_cod'] = $box->user_cod;
+//        $data['status'] = $box->status;
+//        $data['time'] = $box->time;
+        
+        return $this->render('manager',$box->getAttributes());
     }
     
     public function actionNext() {
         // меняем статус бокса на free, если было waiting или work то клиенту done
         $number = Yii::$app->request->post('box_id');
         
+        
         $box = Boxes::findOne($number);
+        $user_cod = $box->user_cod;
         $box->status = 'free';
+        $box->user_cod = '';
+        $box->time =  date('H:i:s');
+        $box->update(FALSE);
+        
+        if ($user_cod) {
+            $client = Clients::findOne($box->user_id);
+            $client->status = 'done';
+            $client->update(FALSE);
+        }
+ 
+    }
+    
+    public function actionWork() {
+        // меняем статус бокса и клиента на working
+        
+        $number = Yii::$app->request->post('box_id');
+        
+        $box = Boxes::findOne($number);
+        $box->status = 'working';
+        $box->time =  date('H:i:s');
         $box->update(FALSE);
         
         $client = Clients::findOne($box->user_id);
         if ($client) {
-            $client->status = 'done';
+            $client->status = 'working';
             $client->update(FALSE);
         }
-        
-        
     }
     
-    public function actionWork($box_id) {
-        // меняем статус бокса и клиента на working
-        
-    }
-    
-    public function actionDeactiv($box_id) {
+    public function actionDeactiv() {
         // меняем статус бокса на deactiv
+        $number = Yii::$app->request->post('box_id');
+        
+        $box = Boxes::findOne($number);
+        $box->status = 'deactiv';
+        $box->user_cod = '';
+        $box->time =  date('H:i:s');
+        $box->update(FALSE);
+        
+    }
+    
+    public function actionBoxitems() {
+        
+        Yii::$app->request->post('box_id');
+        
+        $box = Boxes::findOne($number);
+        
+        echo json_encode($box->getAttributes());die;
+        
         
     }
 
--- backend/views/panel/manager.php
+++ backend/views/panel/manager.php
@@ -7,9 +7,9 @@
 
 <div style="text-align: center" class="small-box bg-aqua">
             <div class="inner">
-              <h3><?= $user_cod ?></h3>
+                <h3 id="user_cod"><?= $user_cod ?></h3>
 
-              <p><?= $status ?></p>
+              <p id="user_status"><?= $status ?></p>
             </div>
             <div class="icon">
               <i class="ion ion-bag"></i>
@@ -23,26 +23,47 @@
       <button type="button" class="btn btn-default" onclick="clickNext(<?= $id ?>)">Следующий клиент</button>
   </div>
   <div class="btn-group">
-    <button type="button" class="btn btn-default">В работу</button>
+    <button type="button" class="btn btn-default" onclick="clickWork(<?= $id ?>)">В работу</button>
   </div>
   <div class="btn-group">
-    <button type="button" class="btn btn-default">Деактивировать</button>
+    <button type="button" class="btn btn-default" onclick="clickDeactiv(<?= $id ?>)">Деактивировать</button>
   </div>
 </div>
 
 
 <script>
 function clickNext(boxNumber){
-    $.post(
-  "<?= \yii\helpers\Url::toRoute(['panel/next','number' => $id]) ?>",
-  {
-    box_id: boxNumber
-
-  }
-);
+    $.post("<?= \yii\helpers\Url::toRoute(['panel/next','number' => $id]) ?>",{box_id: boxNumber});
+    
+    
+//    $.ajax({
+//    url: "<?= \yii\helpers\Url::toRoute(['panel/next','number' => $id]) ?>",
+//     data: boxNumber,
+//    success: function (result) {
+//        //output = result; 
+//        
+//        //var obj = jQuery.parseJSON(result);
+//        alert(jQuery.parseJSON(result).status)
+//        //$("user_cod").innerHtml(obj.user_cod);
+//        //$("user_status").innerHtml(obj.status);
+//        
+//    }/*,
+//    error: function failCallBk(XMLHttpRequest, textStatus, errorThrown) {
+//        alert("An error occurred while processing your request. Please try again.");
+//    }*/
+//});
+    
+    
+    
+    
+    
 }    
-    
-    
+function clickWork(boxNumber){
+    $.post("<?= \yii\helpers\Url::toRoute(['panel/work','number' => $id]) ?>",{box_id: boxNumber});
+}  
+function clickDeactiv(boxNumber){
+    $.post("<?= \yii\helpers\Url::toRoute(['panel/deactiv','number' => $id]) ?>",{box_id: boxNumber});
+}      
     
     
 </script>
